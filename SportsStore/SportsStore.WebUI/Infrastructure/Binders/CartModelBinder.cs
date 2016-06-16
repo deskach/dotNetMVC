dcdd3b4e90b0cef8e17f08841e051a2ffcd8ac37
deskach <dzianis.kachanau@gmail.com>
2016-06-17 09:22:10 +1000

    Model Bionding for Cart was introduced to extract/create the cart from Session

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+/*  Model Binding: Edit Application_Start method of Global.asax 
+    to tell the MVC Framework that it should use CartModelBinder class to create instances of Cart. 
+*/
+
+namespace SportsStore.WebUI.Infrastructure.Binders {
+    public class CartModelBinder : IModelBinder {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext) {
+            Cart cart = (Cart) controllerContext.HttpContext.Session[sessionKey]; ;
+
+            if (cart == null) {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
